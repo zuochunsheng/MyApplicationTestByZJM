74063622a37dc535c9838608cc5875e1ffb281a6
zuochunsheng <1059737741@qq.com>
2017-11-15 08:55:03 +0800

    清理

--- app/src/main/java/com/example/edz/activity/FixedActivity.java
+++ app/src/main/java/com/example/edz/activity/FixedActivity.java
@@ -2,15 +2,8 @@ package com.example.edz.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Environment;
-import android.view.View;
-import android.widget.Toast;
 
-import com.android.annotations.FixMtd;
 import com.example.edz.R;
-import com.mogujie.aceso.Aceso;
-
-import java.io.File;
 
 
 /*
@@ -24,20 +17,5 @@ public class FixedActivity extends Activity {
         setContentView(R.layout.activity_fixed);
     }
 
-    public void fix(View view) {
-        File patchFile = new File(Environment.getExternalStorageDirectory(), "fix.apk");
-        if (!patchFile.exists()) {
-            Toast.makeText(this, "hotfix file not exist!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        File optDir = new File(this.getFilesDir(), "fix_opt");
-        optDir.mkdirs();
-        new Aceso().installPatch(optDir, patchFile);
-    }
-
-    @FixMtd
-    public void test(View view) {
-        Toast.makeText(this, "has been fixed !", Toast.LENGTH_SHORT).show();
-    }
 
 }
